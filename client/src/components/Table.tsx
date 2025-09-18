42f3a5202f12a310638c27c9171c76e44906f6a7
fallerfare <joshualouise.villanueva@g.msuiit.edu.ph>
2025-09-18 16:17:12 +0800

    search-ui, imma switch to serverside searching tho

--- client/src/components/Table.tsx
+++ client/src/components/Table.tsx
@@ -1,8 +1,9 @@
-import { Box } from "@chakra-ui/react"
+import { Box, Heading } from "@chakra-ui/react"
 import { flexRender } from "@tanstack/react-table"
 import '../style/App.css'
 import '../TableConfig.tsx'
 import { getTable } from "../TableConfig.tsx"
+import Filters from "./Filters.tsx"
 
 type TableProps = {
     tableName: "students" | "programs" | "colleges"
@@ -16,76 +17,88 @@ const Table = ({ tableName }: TableProps) => {
     // ========================== 
     // TABLE
     // ==========
-    <Box className="table-card">
-        <Box className="table-scroll">
-            <Box as="table"
-                    className="table"
-                    width="100%">
-            
-                {/* ============ */}
-                {/* Headers */}
-                <Box as="thead">
-                    {table.getHeaderGroups().map((headerGroup) => (
-                        <Box className="tr"
-                                key={headerGroup.id}>
-                            {headerGroup.headers.map((header) => (
-                                <Box className="th"
-                                        key={header.id}
-                                        flex={(header.column.columnDef.meta as any)?.flex ?? 1}
-                                        flexBasis={header.getSize()}>
-                                    {
-                                        flexRender(
-                                            header.column.columnDef.header,
-                                            header.getContext()
-                                        )
-                                    }
-                                    <Box className={`resizer ${
-                                                                    header.column.getIsResizing()
-                                                                     ? "isResizing"
-                                                                     : ""}`}
-                                            onMouseDown={header.getResizeHandler()}
-                                            onTouchStart={header.getResizeHandler()}/>
-                                </Box>
-                            ))}
-                        </Box>
-                    ))}
-                </Box>
-                 {/* Headers */}
-                {/* ============ */}
-            
-                {/* ============ */}
-                {/* Rows */}
-                <Box as="tbody">
-                    {table.getRowModel().rows.map((row) => (
-                            <Box as="tr"
-                                    className="tr-rows"
-                                    key={row.id}>
-                                {row.getVisibleCells().map((cell) => (
-                                    <Box as="td"
-                                            className="td"
-                                            key={(cell.id)}
-                                            flex={(cell.column.columnDef.meta as any)?.flex ?? 1}
-                                            flexBasis={cell.column.getSize()}>
+    <Box>
+        <Box className="heading-card">
+            <Heading as="h1">
+                {tableName}
+            </Heading>
+            <Filters
+                columnFilters={table.getState().columnFilters}
+                setColumnFilters={table.setColumnFilters}
+            />
+        </Box>
+        <Box className="table-card">
+            <Box className="table-scroll">
+                <Box as="table"
+                        className="table"
+                        width="100%">
+        
+                    {/* ============ */}
+                    {/* Headers */}
+                    <Box as="thead">
+                        {table.getHeaderGroups().map((headerGroup) => (
+                            <Box className="tr"
+                                    key={headerGroup.id}>
+                                {headerGroup.headers.map((header) => (
+                                    <Box className="th"
+                                            key={header.id}
+                                            flex={(header.column.columnDef.meta as any)?.flex ?? 1}
+                                            flexBasis={header.getSize()}>
                                         {
                                             flexRender(
-                                                cell.column.columnDef.cell,
-                                                cell.getContext()
+                                                header.column.columnDef.header,
+                                                header.getContext()
                                             )
                                         }
+                                        <Box className={`resizer ${
+                                                                        header.column.getIsResizing()
+                                                                         ? "isResizing"
+                                                                         : ""}`}
+                                                onMouseDown={header.getResizeHandler()}
+                                                onTouchStart={header.getResizeHandler()}/>
                                     </Box>
                                 ))}
                             </Box>
-                        ))
-                    }
+                        ))}
+                    </Box>
+                     {/* Headers */}
+                    {/* ============ */}
+        
+                    {/* ============ */}
+                    {/* Rows */}
+                    <Box as="tbody">
+                        {table.getRowModel().rows.map((row) => (
+                                <Box as="tr"
+                                        className="tr-rows"
+                                        key={row.id}>
+                                    {row.getVisibleCells().map((cell) => (
+                                        <Box as="td"
+                                                className="td"
+                                                key={(cell.id)}
+                                                flex={(cell.column.columnDef.meta as any)?.flex ?? 1}
+                                                flexBasis={cell.column.getSize()}>
+                                            {
+                                                flexRender(
+                                                    cell.column.columnDef.cell,
+                                                    cell.getContext()
+                                                )
+                                            }
+                                        </Box>
+                                    ))}
+                                </Box>
+                            ))
+                        }
+                    </Box>
+                    {/* Rows */}
+                    {/* ============ */}
                 </Box>
-                {/* Rows */}
-                {/* ============ */}
             </Box>
         </Box>
     </Box>
     // ========== 
     // TABLE
    // ==========================
+    
     )
 }
 
