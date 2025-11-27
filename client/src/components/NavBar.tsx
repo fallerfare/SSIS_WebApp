26e974e48f193b046532374bd6e4f9c0a078b041
fallerfare <joshualouise.villanueva@g.msuiit.edu.ph>
2025-11-27 16:12:00 +0800

    UI Fixes

--- client/src/components/NavBar.tsx
+++ client/src/components/NavBar.tsx
@@ -2,36 +2,51 @@ import Container from 'react-bootstrap/Container'
 import Nav from 'react-bootstrap/Nav'
 import Navbar from 'react-bootstrap/Navbar'
 import NavDropdown from 'react-bootstrap/NavDropdown'
-import { Link } from "react-router-dom"
+import { NavLink, useLocation } from "react-router-dom"
 
 function NavBar() {
+
+  const location = useLocation()
+
+  const isRecordsActive = [
+    "/table/students",
+    "/table/programs",
+    "/table/colleges",
+  ].includes(location.pathname)
+
+  const isAcademicActive = [
+    "/enrollment",
+    "/establish/programs",
+    "/establish/colleges",
+  ].includes(location.pathname)
+  
   return (
     <Navbar expand='lg' className='navbar-custom'>
       <Container className='navbar-container'>
-        <Navbar.Brand href='#home' id='navbar-brand'/>
+        <Navbar.Brand id='navbar-brand'/>
         <Navbar.Toggle aria-controls='basic-navbar-nav' />
         <Navbar.Collapse id='basic-navbar-nav'>
           <Nav className='mx-auto' id='navbar-links'>
             <Nav.Item>
-              <Nav.Link as={Link} to="/profile">
+              <Nav.Link as={NavLink} to="/profile">
                 profile
               </Nav.Link>
             </Nav.Item>
             <Nav.Item>
-              <NavDropdown title='records' id='basic-nav-dropdown'>
+              <NavDropdown title='records' id='basic-nav-dropdown' className={isRecordsActive ? "active-dropdown" : ""}>
                 <div className='nav-dropdown'>
                 <NavDropdown.Item>
-                  <Nav.Link as={Link} to="/table/students">
+                  <Nav.Link as={NavLink} to="/table/students">
                     students
                   </Nav.Link>
                 </NavDropdown.Item>
                 <NavDropdown.Item>
-                  <Nav.Link as={Link} to="/table/programs">
+                  <Nav.Link as={NavLink} to="/table/programs">
                     programs
                   </Nav.Link>
                 </NavDropdown.Item>
                 <NavDropdown.Item>
-                  <Nav.Link as={Link} to="/table/colleges">
+                  <Nav.Link as={NavLink} to="/table/colleges">
                     colleges
                   </Nav.Link>
                 </NavDropdown.Item>
@@ -39,20 +54,20 @@ function NavBar() {
               </NavDropdown>
             </Nav.Item>
             <Nav.Item>
-              <NavDropdown title='academic affairs' id='basic-nav-dropdown'>
+              <NavDropdown title='academic affairs' id='basic-nav-dropdown' className={isAcademicActive ? "active-dropdown" : ""}>
                 <div className='nav-dropdown'>
                 <NavDropdown.Item>
-                  <Nav.Link as={Link} to="/enrollment">
+                  <Nav.Link as={NavLink} to="/enrollment">
                     admission
                   </Nav.Link>
                 </NavDropdown.Item>
                 <NavDropdown.Item>
-                  <Nav.Link as={Link} to="/establish/programs">
+                  <Nav.Link as={NavLink} to="/establish/programs">
                     create program
                   </Nav.Link>
                 </NavDropdown.Item>
                 <NavDropdown.Item>
-                  <Nav.Link as={Link} to="/establish/colleges">
+                  <Nav.Link as={NavLink} to="/establish/colleges">
                     create college
                   </Nav.Link>
                 </NavDropdown.Item>
